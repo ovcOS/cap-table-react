65c0267735166ca1bf7b68d46c98f86a569c16ff
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-01-31 12:55:43 +0100

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import piggyBank from '../icons/piggyBank.png'
 
 const Header = () => (
-  <header>
-    <h1 className="app-header">
-      THE CAP TABLE APP
-    </h1>
+  <header className="app-header">
+    <div className="header-box">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <div className="header-content">
+        <h1>THE CAP TABLE APP</h1>
+      </div>
+    </div>
+      <img src={piggyBank} className="piggy-bank" alt="/"/>
   </header>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
+import './css/global.css';
+import './css/header.css'
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
